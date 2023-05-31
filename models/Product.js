ec8dda74b68a9d1e5f81ef453afdfc0e779440f5
zscheel <zachary.scheel1@gmail.com>
2023-05-30 19:06:21 -0500

    added code to define colums in product.js and producttag.js

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,47 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      product_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        validate: {
+          isDec: function(num) {
+            if (num % 1 === 0) {
+              return true;
+            }
+            return false;
+          }
+        }
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isNumeric: function(num) {
+            if (typeof num == 'number') {
+              return true;
+            }
+            return false;
+          }
+        }
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id'
+        }
+      }
     // define columns
   },
   {
